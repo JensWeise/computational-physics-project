31ab65fb122fbbdda7d37453e3110475c79718b4
JensWeise <juiyenisme@gmail.com>
2023-05-03 22:33:00 +0800

    1

---
+++ badminton.py
@@ -0,0 +1,18 @@
+import vpython as vp 
+
+vp.canvas(width=600, height=600)
+
+#shuttlecock
+head = vp.sphere(pos=vp.vector(0,0,0), radius=1.4, make_trail=True)
+feather = vp.cone(pos=vp.vector(-7,0,0), radius=3.4, axis=vp.vector(7,0,0))
+
+shuttlecock = vp.compound([head,feather])
+
+#racket
+racket_head = vp.ellipsoid(pos=vp.vector(100,0,0), lenght=1, width=23, height=29)
+bat = vp.cylinder(pos=vp.vector(100,0,0), radius=0.5, axis=vp.vector(0,-53.5,0))
+racket = vp.compound([racket_head,bat])
+
+#spin
+
+
