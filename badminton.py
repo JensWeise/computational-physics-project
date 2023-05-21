d6a5a15b9860b24902050015065f80c404d2cfdd
JensWeise <juiyenisme@gmail.com>
2023-05-21 21:28:27 +0800

    idk

--- badminton.py
+++ badminton.py
@@ -5,9 +5,9 @@ canvas(title="badminton's movement",width=600, height=600, autoscale=True)
 
 #initial parameter
 theta = pi/4 #發射仰角
-v0 = 30
+v0 = 30 #初數
 g = vector(0,-9.8,0)
-m = 5.50e-3
+m = 5.50e-3 #羽球重量
 
 #shuttlecock
 head = sphere(pos=vector(0,0,0), radius=1.4)
@@ -23,12 +23,12 @@ racket = compound([racket_head,bat], axis=vector(0,-53.5,0), pos=vec(100,0,0), o
 
 #parameter
 vel = vector(v0*cos(theta), v0*sin(theta), 0)
-w = 10*pi*norm(shuttlecock.axis) #angular velocity
-ww = 50*norm(shuttlecock.axis)
+w = 10*pi*norm(shuttlecock.axis) #自旋
+ww = 50*norm(shuttlecock.axis) #螺線
 t = 0
 dt = 0.01
 T = 100
-eta = 1.81e-5
+eta = 1.81e-5 #流體黏滯係數
 b = -6*pi*eta*4.33
 
 ws = 10*vector(0,0,1)
